38cd0e2565c06bffa8c66897cb278a40c20aa121
ayushgaikwad26 <ayushgaikwad48@gmail.com>
2025-03-06 15:28:45 +0530

    Update main.cpp

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+using namespace std;
 
 int main() {
 
+    string player_name;
+
+    cout << "Enter Your Player Name" << endl;
+    cin >> player_name;
+
+    cout << "Congrats Your Player Name is " << player_name << endl;
     return 0;
 }
