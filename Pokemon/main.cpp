9f4bd89886f1f10cf8c81e59fcf0f48633b3f7b5
ayushgaikwad26 <ayushgaikwad48@gmail.com>
2025-03-11 12:38:47 +0530

    ,
    ,

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,3 +1,5 @@
+#include "PokemonType.hpp"
+#include "PokemonChoice.hpp"
 #include <iostream>
 #include <string>
 using namespace std;
@@ -26,13 +28,6 @@ enum class PokemonChoice {
 };
 
 
-enum class PokemonType {
-    FIRE,
-    GRASS,
-    WATER,
-    ELECTRIC,
-    NORMAL 
-};
 
 
 class Pokemon {
@@ -129,20 +124,20 @@ public:
     void offerPokemonChoices(Player& player) {
         cout
             << name
-            << ": First, tell me, what’s your name? \t [Please Enter Your Name]\n";
+            << ": First, tell me, whatï¿½s your name? \t [Please Enter Your Name]\n";
         getline(std::cin, player.name);
         cout << name << ": Ah, " << player.name
             << "! What a fantastic name!\n";
         waitForEnter();
         cout << name
             << ": You must be eager to start your adventure. But first, "
-            "you’ll need a Pokemon of your own!\n";
+            "youï¿½ll need a Pokemon of your own!\n";
         waitForEnter();
 
         
         cout
             << name
-            << ": I have three Pokemon here with me. They’re all quite feisty!\n";
+            << ": I have three Pokemon here with me. Theyï¿½re all quite feisty!\n";
         waitForEnter();
         cout << name << ": Choose wisely...\n";
         cout << "1. Charmander - The fire type. A real hothead!\n";
@@ -168,50 +163,50 @@ public:
             << "!, I am about to explain you about your upcoming grand "
             "adventure.\n";
         waitForEnter();
-        cout << "Professor Oak: You see, becoming a Pokémon Master is no easy "
+        cout << "Professor Oak: You see, becoming a Pokï¿½mon Master is no easy "
             "feat. It takes courage, wisdom, and a bit of luck!\n";
         waitForEnter();
         cout
-            << "Professor Oak: Your mission, should you choose to accept it—and "
-            "trust me, you really don’t have a choice—is to collect all the "
-            "Pokémon Badges and conquer the Pokémon League.\n";
+            << "Professor Oak: Your mission, should you choose to accept itï¿½and "
+            "trust me, you really donï¿½t have a choiceï¿½is to collect all the "
+            "Pokï¿½mon Badges and conquer the Pokï¿½mon League.\n";
         waitForEnter();
 
         cout << "\n"
             << player.name
-            << ": Wait... that sounds a lot like every other Pokémon game "
+            << ": Wait... that sounds a lot like every other Pokï¿½mon game "
             "out there...\n";
         waitForEnter();
         cout << "Professor Oak: Shhh! Don't break the fourth wall, "
             << player.name << "! This is serious business!\n";
         waitForEnter();
 
-        cout << "\nProfessor Oak: To achieve this, you’ll need to battle wild "
-            "Pokémon, challenge gym leaders, and of course, keep your "
-            "Pokémon healthy at the PokeCenter.\n";
+        cout << "\nProfessor Oak: To achieve this, youï¿½ll need to battle wild "
+            "Pokï¿½mon, challenge gym leaders, and of course, keep your "
+            "Pokï¿½mon healthy at the PokeCenter.\n";
         waitForEnter();
-        cout << "Professor Oak: Along the way, you'll capture new Pokémon to "
-            "strengthen your team. Just remember—there’s a limit to how "
-            "many Pokémon you can carry, so choose wisely!\n";
+        cout << "Professor Oak: Along the way, you'll capture new Pokï¿½mon to "
+            "strengthen your team. Just rememberï¿½thereï¿½s a limit to how "
+            "many Pokï¿½mon you can carry, so choose wisely!\n";
         waitForEnter();
 
         cout << "\n"
             << player.name << ": Sounds like a walk in the park... right?\n";
         waitForEnter();
-        cout << "Professor Oak: Hah! That’s what they all say! But beware, "
+        cout << "Professor Oak: Hah! Thatï¿½s what they all say! But beware, "
             "young Trainer, the path to victory is fraught with "
-            "challenges. And if you lose a battle... well, let’s just say "
+            "challenges. And if you lose a battle... well, letï¿½s just say "
             "you'll be starting from square one.\n";
         waitForEnter();
 
         cout << "\nProfessor Oak: So, what do you say? Are you ready to "
-            "become the next Pokémon Champion?\n";
+            "become the next Pokï¿½mon Champion?\n";
         waitForEnter();
-        cout << "\n" << player.name << ": Ready as I’ll ever be, Professor!\n";
+        cout << "\n" << player.name << ": Ready as Iï¿½ll ever be, Professor!\n";
         waitForEnter();
 
         cout
-            << "\nProfessor Oak: That’s the spirit! Now, your journey begins...\n";
+            << "\nProfessor Oak: Thatï¿½s the spirit! Now, your journey begins...\n";
         waitForEnter();
         cout << "Professor Oak: But first... let's just pretend I didn't "
             "forget to set up the actual game loop... Ahem, onwards!\n";
@@ -230,10 +225,10 @@ void gameLoop(Player& player) {
 
         
         cout << "\nWhat would you like to do next, " << player.name << "?\n";
-        cout << "1. Battle Wild Pokémon\n";
+        cout << "1. Battle Wild Pokï¿½mon\n";
         cout << "2. Visit PokeCenter\n";
         cout << "3. Challenge Gyms\n";
-        cout << "4. Enter Pokémon League\n";
+        cout << "4. Enter Pokï¿½mon League\n";
         cout << "5. Quit\n";
         cout << "Enter your choice: ";
         cin >> choice;
@@ -245,25 +240,25 @@ void gameLoop(Player& player) {
         
         switch (choice) {
         case 1:
-            cout << "You look around... but all the wild Pokémon are on "
+            cout << "You look around... but all the wild Pokï¿½mon are on "
                 "vacation. Maybe try again later?\n";
             break;
         case 2:
             cout
                 << "You head to the PokeCenter, but Nurse Joy is out on a coffee "
-                "break. Guess your Pokémon will have to tough it out for now!\n";
+                "break. Guess your Pokï¿½mon will have to tough it out for now!\n";
             break;
         case 3:
             cout << "You march up to the Gym, but it's closed for renovations. "
                 "Seems like even Gym Leaders need a break!\n";
             break;
         case 4:
-            cout << "You boldly step towards the Pokémon League... but the "
+            cout << "You boldly step towards the Pokï¿½mon League... but the "
                 "gatekeeper laughs and says, 'Maybe next time, champ!'\n";
             break;
         case 5:
             cout << "You try to quit, but Professor Oak's voice echoes: "
-                "'There's no quitting in Pokémon training!'\n";
+                "'There's no quitting in Pokï¿½mon training!'\n";
             cout << "Are you sure you want to quit? (y/n): ";
             char quitChoice;
             cin >> quitChoice;
